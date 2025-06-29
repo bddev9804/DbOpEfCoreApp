d32ff6ef397e5e5e023dc6951e11bdb5a3d27e47
Bibek <bibekshaw249@gmail.com>
2025-06-29 22:02:16 +0530

    With Gitignore

---
+++ EfCoreApp/EfCoreApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        }
+        
+    }
+}
--- EfCoreApp/EfCoreApp/Program.cs
+++ EfCoreApp/EfCoreApp/Program.cs
@@ -1,4 +1,7 @@
 
+using EfCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace EfCoreApp
 {
     public class Program
@@ -8,7 +11,9 @@ namespace EfCoreApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+            );
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
